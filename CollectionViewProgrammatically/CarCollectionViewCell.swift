4d9979cea54a0e31c088440099e7bbb4540737f3
Abraham Tesfamariam <abe973t@gmail.com>
2020-01-27 15:51:46 -0500

    add car images

---
+++ CollectionViewProgrammatically/CarCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CarCollectionViewCell.swift
+//  CollectionViewProgrammatically
+//
+//  Created by mcs on 1/27/20.
+//  Copyright © 2020 mcs. All rights reserved.
+//
+
+import UIKit
+
+class CarCollectionViewCell: UICollectionViewCell {
+    
+}
